4cbcf4722412b5e917ff2d8ce04233a2c67ad5ab
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2014-09-04 11:54:47 +0100

    cas ontology

--- glossary.js
+++ glossary.js
@@ -6,134 +6,73 @@
 glossary_string= 
 ' ' + 
 '<html> ' + 
+'<!-- ' + 
+'authentication_token ' + 
+'plan ' + 
+'acknowledgement ' + 
+'machine_output ' + 
+'request ' + 
+'negotiation_offer ' + 
+'feedback_report ' + 
+'reputation_report ' + 
+'ui_view ' + 
+'incentive_reward ' + 
 ' ' + 
-'<div class="glossary" id="glossary-article">   ' + 
-'An <dfn id="concept-article" title="Article">article</dfn> ' + 
-' such as a news article or piece of investigative report. Newspapers and magazines have articles of many different types and this is intended to cover them all ' + 
-'(same as <a href="http://schema.org/Article">schema:Article</a>). ' + 
+'authenticate_agent ' + 
+'submit_request ' + 
+'generate_plan ' + 
+'modify_plan ' + 
+'store_data ' + 
+'generate_reputation ' + 
+'submit_ offer ' + 
+'change_view ' + 
+'reward_incentive ' + 
+' ' + 
+'--> ' + 
+' ' + 
+' ' + 
+' ' + 
+'<div class="glossary" id="glossary-plan">   ' + 
+'A <dfn id="concept-plan" title="Plan">plan</dfn> ... ' + 
 '</div>   ' + 
 ' ' + 
-'<div class="glossary" id="glossary-book">   ' + 
-'A <dfn id="concept-book" title="Book">book</dfn> ' + 
-'(same as <a href="http://schema.org/Book">schema:Book</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-figure">   ' + 
-'A <dfn id="concept-figure" title="Figure">figure</dfn> is a diagram or illustrative drawing ' + 
-'(same as <a href="http://schema.org/ImageObject">schema:ImageObject</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-dataset">   ' + 
-'A <dfn id="concept-dataset" title="Dataset">dataset</dfn> is a body of structured information describing some topic(s) of interest ' + 
-'(same as <a href="http://schema.org/Dataset">schema:Dataset</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-code">   ' + 
-'<dfn id="concept-code" title="Code">Code</dfn>  ' + 
-'means computer programming source code. Example: Full (compile ready) solutions, code snippet samples, scripts, templates ' + 
-'(same as <a href="http://schema.org/Code">schema:Code</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-slides">   ' + 
-'<dfn id="concept-slides" title="Slides">Slides</dfn> are electronic presentations consisting of a series of separate pages or slides. ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-citation">   ' + 
-'A <dfn id="concept-citation" title="Citation">Citation</dfn>  ' + 
-'a reference to another creative work (especially in a scholarly work). ' + 
-'(subtype of <a href="http://www.w3c.org/ns/prov#Derivation">prov:Derivation</a>). ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-author">   ' + 
-'An <dfn id="concept-author" title="Author">Author</dfn>  ' + 
-'is the <a href="http://www.w3c.org/ns/prov#Agent">prov:Agent</a>  to which  some creative work is attributed as its author. ' + 
+'<div class="glossary" id="glossary-incentive">   ' + 
+'A <dfn id="concept-incentive" title="Incentive">incentive</dfn> ... ' + 
 '</div>   ' + 
 ' ' + 
 ' ' + 
+'<div class="glossary" id="glossary-reward">   ' + 
+'A <dfn id="concept-reward" title="Reward">reward</dfn> ... ' + 
+'</div>   ' + 
 ' ' + 
-'<div class="glossary" id="glossary-reviewer">   ' + 
-'A <dfn id="concept-reviewer" title="Reviewer">Reviewer</dfn> ' + 
-' a person who writes critical appraisals of creative works. It  ' + 
-'is the <a href="http://www.w3c.org/ns/prov#Agent">prov:Agent</a>  to which  some creative work is attributed as its reviewer. ' + 
+'<div class="glossary" id="glossary-credential">   ' + 
+'A <dfn id="concept-credential" title="Credential">credential</dfn> ... ' + 
 '</div>   ' + 
 ' ' + 
 ' ' + 
+'<div class="glossary" id="glossary-acknowledgement">   ' + 
+'An <dfn id="concept-acknowledgement" title="Acknowledgement">acknowledgement</dfn> is ... ' + 
+'</div>   ' + 
 ' ' + 
-'<div class="glossary" id="glossary-contributor"> ' + 
-'A <dfn id="concept-contributor" title="Contributor">Contributor</dfn> ' + 
-'a person who contributes to some creative work, but the nature of the ' + 
-'contribution is not specific. It is ' + 
-'the <a href="http://www.w3c.org/ns/prov#Agent">prov:Agent</a> to which ' + 
-'some creative work is attributed as its contributor. ' + 
+'<div class="glossary" id="glossary-request">   ' + 
+'An <dfn id="concept-request" title="Request">request</dfn> is ... ' + 
+'</div>   ' + 
+' ' + 
+'<div class="glossary" id="glossary-feedback">   ' + 
+'A <dfn id="concept-feedback" title="Feedback">feedback</dfn> is a report that is derived from an event or experience, and consists of a set of key value pairs, which binds a topic of reputation to a rating value. For example, a participant has taken part in an event and leaves feedback for about their “star rating = 5”. ' + 
 '</div>   ' + 
 ' ' + 
 ' ' + 
-'<div class="glossary" id="glossary-funder"> ' + 
-'A <dfn id="concept-funder" title="Funder">Funder</dfn> ' + 
-'a person or organization who financially  supported  some creative work. It is ' + 
-'the <a href="http://www.w3c.org/ns/prov#Agent">prov:Agent</a> to which ' + 
-'some creative work is attributed as its funder. ' + 
+'<div class="glossary" id="glossary-reputation">   ' + 
+'A <dfn id="concept-reputation" title="Reputation">reputation</dfn> is a report about a subject that contains a set of key value pairs. The key value pairs is a binding between a topic of reputation to a rating value, which is an aggregation of a set ratings for that topic. ' + 
+'For example, a subject has been rated with a set of star ratings, and the average of those star ratings are "average star rating = 4.3". ' + 
 '</div>   ' + 
 ' ' + 
 ' ' + 
-'<div class="glossary" id="glossary-editor">   ' + 
-'An <dfn id="concept-editor" title="Editor">Editor</dfn>  ' + 
-'is the <a href="http://www.w3c.org/ns/prov#Agent">prov:Agent</a>  to which  some creative work is attributed as its editor. ' + 
+'<div class="glossary" id="glossary-opinion">   ' + 
+'A <dfn id="concept-opinion" title="Opinion">opinion</dfn> is a specialisation of a reputation report, which contains a single key value pair and binds an opinion to a value. The value is an aggregation of ratings from a set of reputation topics. ' + 
 '</div>   ' + 
 ' ' + 
-' ' + 
-'<div class="glossary" id="glossary-scheduledsession">   ' + 
-'A <dfn id="concept-scheduledsession" title="ScheduledSession">ScheduledSession</dfn>  ' + 
-'is a planned period devoted to a particular activity (subtype of <a href="http://www.w3c.org/ns/prov#Plan">prov:Plan</a>). ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-presentationactivity">   ' + 
-'A <dfn id="concept-presentationactivity" title="PresentationActivity">PresentationActivity</dfn> is an activity during which some creative work is presented to an audience (subtype of <a href="http://www.w3c.org/ns/prov#Activity">prov:Activity</a>). ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-table">   ' + 
-'A <dfn id="concept-table" title="Table">table</dfn> a set of facts or figures systematically displayed, especially in columns. ' + 
-'(same as <a href="http://schema.org/Table">schema:Table</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-poster">   ' + 
-'A <dfn id="concept-poster" title="Poster">poster</dfn> ' + 
-'is a large, usually printed placard, bill, or announcement, often illustrated, that is posted to advertise or publicize something. ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-tweet">   ' + 
-'A <dfn id="concept-tweet" title="Tweet">tweet</dfn> is ' + 
-'a posting made on the social media website Twitter. ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-bindingsderivation">   ' + 
-'A <dfn id="concept-bindingsderivation" title="BindingsDerivation">BindingsDerivation</dfn> ' + 
-'is the transformation that led to some document given a set of bindings (subtype of <a href="http://www.w3c.org/ns/prov#Derivation">prov:Derivation</a>). ' + 
-'</div>   ' + 
-' ' + 
-'<div class="glossary" id="glossary-mention">   ' + 
-'A <dfn id="concept-mention" title="Mention">mention</dfn> ' + 
-'is a reference to someone or something. e.g. a tweet mentions a presentation. ' + 
-'(subtype of <a href="http://www.w3c.org/ns/prov#Derivation">prov:Derivation</a>). ' + 
-'</div>   ' + 
-' ' + 
-' ' + 
-'<div class="glossary" id="glossary-expansion">   ' + 
-'An <dfn id="concept-expansion" title="Expansion">expansion</dfn> ' + 
-'is the transformation that lead to some document after expansion of a template ' + 
-'(subtype of <a href="http://www.w3c.org/ns/prov#Derivation">prov:Derivation</a>). ' + 
-'</div>   ' + 
 ' ' + 
 ' ' + 
 '</html> ' + 
